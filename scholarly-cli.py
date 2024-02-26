b25dfbc2e9cf491aeea2b00ab46239bb5cebfbfd
bjohas <bjohas+git@gmail.com>
2024-02-26 19:52:23 +0000

    Update to scholarly-cli.py

--- scholarly-cli.py
+++ scholarly-cli.py
@@ -17,8 +17,10 @@ parser.add_argument('--query', type=str, help='Search query', required=True)
 parser.add_argument('--results', type=int, default=20, help='Number of results to retrieve')
 #parser.add_argument('--format', type=str, choices=['json', 'bibtex'], default='json', help='Output format')
 parser.add_argument('--json', type=bool, default=True, help='Output json (default=True).')
+parser.add_argument('--ijson', type=bool, default=False, help='Output individual json files, one per result (default=False).')
 parser.add_argument('--bibtex', type=bool, default=False, help='Output bibtex (default=False).')
 parser.add_argument('--out', type=str, help='Output file name without extension - otherwise the search query will be used')
+parser.add_argument('--time', type=bool, default=True, help='Prefix data/time to the output file.')
 
 """
 # These areguments should also be included:
@@ -38,11 +40,15 @@ Also support:
 
 settings = {}
 args = parser.parse_args()
-settings["start"] = gettime()
+settings["time_start"] = gettime()
 settings["args"] = vars(args)
 
 if (not(args.json or args.bibtex)):
     print("No output will be produced! Use --json or --bibtex to produce output.")
+
+if not args.json and args.ijson:
+    print("For --ijson to work, please do not turn off --json.")
+    exit()
 
 timestamp("Registering proxy:")
 
@@ -82,6 +88,10 @@ if (args.out):
 else:
     outfile = f"{args.query}"
 
+if (args.time):
+    settings["timestamp"] = gettime()
+    outfile = settings["timestamp"] + " - " + outfile
+
 timestamp("Output file name: "+outfile)
 
 timestamp("Querying...")
@@ -98,54 +108,34 @@ for i, result in enumerate(search_query):
     results.append(result)
 
 timestamp("Enumeration done!")
-settings["end"] = gettime()
+settings["time_end"] = gettime()
 
-with open(f"{outfile}.settings.json", 'w', encoding='utf-8') as f:
-    f.write(json.dumps(settings, indent=4))
 
 alljson = []
 
 if args.json:
     for i, result in enumerate(results):
-        with open(f"{outfile}_{i+1}.json", 'w') as f:
-            json.dump(result, f, indent=4)
+        if args.ijson:
+            with open(f"{outfile}_{i+1}.json", 'w') as f:
+                json.dump(result, f, indent=4)
+        alljson.append(result)
+    settings["results"] = alljson
+    with open(f"{outfile}.json", 'w') as f:
+        json.dump(settings, f, indent=4)
+else:
+    with open(f"{outfile}.settings.json", 'w', encoding='utf-8') as f:
+        f.write(json.dumps(settings, indent=4))
+
 
 if args.bibtex:
     bibtex_results = ""
     for i, result in enumerate(results):
         bibtex = scholarly.bibtex(result)
+        filename = f"{args.out}_{i+1}.bibtex"
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(bibtex)
         bibtex_results += bibtex + '\n\n'
-
     with open(f"{outfile}.bibtex", 'w', encoding='utf-8') as f:
         f.write(bibtex_results)
-
-if args.json:
-    # Consolidate all JSON results into a single file
-    all_results = []
-
-    for i in range(args.results):
-        filename = f"{args.out}_{i+1}.json"
-        if os.path.exists(filename):
-            with open(filename, 'r') as f:
-                result_data = json.load(f)
-                all_results.append(result_data)
-
-    with open(f"{outfile}.json", 'w') as f:
-        json.dump(all_results, f, indent=4)
-
-if args.bibtex:
-    # Consolidate all bibtex results into a single file
-    all_results = ""
-
-    for i in range(args.results):
-        filename = f"{args.out}_{i+1}.bibtex"
-        if os.path.exists(filename):
-            with open(filename, 'r', encoding='utf-8') as f:
-                result_data = f.read()
-                all_results = all_results + "\n" + result_data
-
-    with open(f"{outfile}.bibtex", 'w', encoding='utf-8') as f:
-        f.write(all_results)
-
 
 timestamp("All done!")

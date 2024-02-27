026723abea4f7eaa4b9296b360c6824dc6b9d008
Adnane Touiyate <125739309+Adnane-markue@users.noreply.github.com>
2024-02-27 11:15:53 +0100

    Refactor scholarly-cli

--- scholarly-cli.py
+++ scholarly-cli.py
@@ -1,141 +1,76 @@
-#!/usr/bin/python3
 import argparse
-import datetime
 import json
 import os
 from scholarly import scholarly, ProxyGenerator
+import time
+import requests
 
-def gettime():
-    return datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--query', type=str, help='Search query')
+    parser.add_argument('--results', type=int, default=20, help='Number of results to retrieve')
+    parser.add_argument('--format', type=str, choices=['json', 'bibtex'], default='json', help='Output format')
+    parser.add_argument('--out', type=str, default='results', help='Output file name without extension')
+    parser.add_argument('--api_key', type=str, help='Scraper API Key')
+    parser.add_argument('--api_key_file', type=str, help='Path to file containing Scraper API Key')
 
-def timestamp(text):
-    print(gettime() + ": " + text)
-    return
+    return parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--query', type=str, help='Search query', required=True)
-parser.add_argument('--results', type=int, default=20, help='Number of results to retrieve')
-#parser.add_argument('--format', type=str, choices=['json', 'bibtex'], default='json', help='Output format')
-parser.add_argument('--json', type=bool, default=True, help='Output json (default=True).')
-parser.add_argument('--ijson', type=bool, default=False, help='Output individual json files, one per result (default=False).')
-parser.add_argument('--bibtex', type=bool, default=False, help='Output bibtex (default=False).')
-parser.add_argument('--out', type=str, help='Output file name without extension - otherwise the search query will be used')
-parser.add_argument('--time', type=bool, default=True, help='Prefix data/time to the output file.')
+def read_api_key(api_key, api_key_file):
+    if api_key_file:
+        with open(api_key_file, 'r') as f:
+            return f.read().strip()
+    else:
+        return api_key
 
-"""
-# These areguments should also be included:
-parser.add_argument('--patents', type=bool, default=True, help='Fill me in.')
-parser.add_argument('--citations', type=bool, default=True, help='Fill me in.')
-parser.add_argument('--year_low', type=int, default=None, help='Fill me in.')
-parser.add_argument('--year_high', type=int, default=None, help='Fill me in.')
-parser.add_argument('sort_by (string, optional) – , help='Fill me in.'‘relevance’ or ‘date’, defaults to ‘relevance’
-parser.add_argument('include_last_year (string, optional) – , help='Fill me in.'‘abstracts’ or ‘everything’, defaults to ‘abstracts’ and only applies if ‘sort_by’ is ‘date’
-parser.add_argument('start_index (int, optional) – , help='Fill me in.'starting index of list of publications, defaults to 0
+def get_full_publication_details(publication):
+    return scholarly.fill(publication)
 
-Also support:
-- fill
-- citedby
-"""
+def main():
+    start_time = time.time()
 
+    args = parse_arguments()
 
-settings = {}
-args = parser.parse_args()
-settings["time_start"] = gettime()
-settings["args"] = vars(args)
+    api_key = read_api_key(args.api_key, args.api_key_file)  
 
-if (not(args.json or args.bibtex)):
-    print("No output will be produced! Use --json or --bibtex to produce output.")
+    pg = ProxyGenerator()
+    if api_key:
+        try:
+            pg.ScraperAPI(api_key)
+        except requests.exceptions.RequestException as e:
+            print(f"Error setting up proxy: {e}")
+            return
+    else:
+        print("No API key provided. Using free proxies.")
+        pg.FreeProxies()
 
-if not args.json and args.ijson:
-    print("For --ijson to work, please do not turn off --json.")
-    exit()
+    scholarly.use_proxy(pg)
 
-timestamp("Registering proxy:")
+    search_query = scholarly.search_pubs(args.query)
+    results = []
 
-scraperapifile = ""
-home = os.path.expanduser("~")
-if os.path.exists("scraperapi.json"):
-    scraperapifile = "scraperapi.json"
-elif os.path.exists(home + "/.config/scraperapi/scraperapi.json"):
-    scraperapifile = home + "/.config/scraperapi/scraperapi.json"
+    print("Start looking for publications in Google Scholar for the query:", args.query)
+    for i, publication in enumerate(search_query):
+        print("Retrieving publication", i + 1, "of", args.results)
+        if i == args.results - 1:
+            break
+        full_publication = get_full_publication_details(publication)
+        results.append(full_publication)
 
-# check wethere there is a file called scraperapi.json
-if os.path.exists(scraperapifile):
-    with open(scraperapifile, 'r') as f:
-        scraperapi = json.load(f)
-else:
-    scraperapi = {}
+    if args.format == 'json':
+        with open('results.json', 'w') as f:
+            json.dump(results, f, indent=4)
+    elif args.format == 'bibtex':
+        bibtex_results = ""
+        for result in results:
+            bibtex = scholarly.bibtex(result)
+            bibtex_results += bibtex + '\n\n'
+        with open('results.bib', 'w', encoding='utf-8') as f:
+            f.write(bibtex_results)
 
-pg = ProxyGenerator()
+    end_time = time.time()
+    duration = end_time - start_time
+    print(f"Script execution time: {duration} seconds")
 
-# check whether scraperapi has the key called key
-if "key" in scraperapi:
-    success = pg.ScraperAPI(scraperapi["key"])
-    if not success:
-        timestamp("failed connecting to scraperapi!")
-        exit()
-    else:  
-        timestamp("connected to scraperapi!")
-else:
-    timestamp("Free proxy will be registered!")
-    pg.FreeProxies()
-    
-scholarly.use_proxy(pg)
-
-outfile = ""
-if (args.out):
-    outfile = f"{args.out}"
-else:
-    outfile = f"{args.query}"
-
-if (args.time):
-    settings["timestamp"] = gettime()
-    outfile = settings["timestamp"] + " - " + outfile
-
-timestamp("Output file name: "+outfile)
-
-timestamp("Querying...")
-search_query = scholarly.search_pubs(args.query)
-results = []
-
-timestamp("Query done! results: " + str(search_query.total_results))
-settings["total_results"] = search_query.total_results
-
-for i, result in enumerate(search_query):
-    timestamp(str(i))
-    if i >= args.results-1:
-        break
-    results.append(result)
-
-timestamp("Enumeration done!")
-settings["time_end"] = gettime()
-
-
-alljson = []
-
-if args.json:
-    for i, result in enumerate(results):
-        if args.ijson:
-            with open(f"{outfile}_{i+1}.json", 'w') as f:
-                json.dump(result, f, indent=4)
-        alljson.append(result)
-    settings["results"] = alljson
-    with open(f"{outfile}.json", 'w') as f:
-        json.dump(settings, f, indent=4)
-else:
-    with open(f"{outfile}.settings.json", 'w', encoding='utf-8') as f:
-        f.write(json.dumps(settings, indent=4))
-
-
-if args.bibtex:
-    bibtex_results = ""
-    for i, result in enumerate(results):
-        bibtex = scholarly.bibtex(result)
-        filename = f"{args.out}_{i+1}.bibtex"
-        with open(filename, 'w', encoding='utf-8') as f:
-            f.write(bibtex)
-        bibtex_results += bibtex + '\n\n'
-    with open(f"{outfile}.bibtex", 'w', encoding='utf-8') as f:
-        f.write(bibtex_results)
-
-timestamp("All done!")
+if __name__ == "__main__":
+    main()
